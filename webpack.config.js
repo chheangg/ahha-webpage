25e06d48dcbb6e4c8a0dd1c300c83329adcff928
chheangg <balldremz@gmail.com>
2022-01-23 11:36:15 +0700

    Add frontpage design

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: "./src/index.js",
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }, 
+        {
+            test: /\.(png|jpeg|jpg|svg|gif)$/i,
+            type: 'asset/resource',
+        }, 
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+        },
+    ],
+    },
+}
